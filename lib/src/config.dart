0d0ce72cbd3703f00a5703322bfd803df4e60f08
NonymousMorlock <agamspaul@gmail.com>
2024-04-16 11:45:14 +0000

    Added Agora and Onesignal appIds

--- lib/src/config.dart
+++
@@ -1,2 +0,0 @@
-const oneSignalAppId = 'YOUR-APP-ID-HERE';
-const agoraAppId = 'YOUR-APP-ID-HERE';
---
+++ lib/src/config.example.dart
@@ -0,0 +1,2 @@
+const oneSignalAppId = 'YOUR_ONE_SIGNAL_APP_ID';
+const agoraAppId = 'YOUR_AGORA_APP_ID';
